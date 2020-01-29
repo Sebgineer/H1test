08418d49b0271e25a8d93662ef825075dd65e181
Sebastian Lind Karlsen (seba6474) <seba6474@zbc.dk>
2020-01-29 09:31:01 +0100

    Add project files.

---
+++ H1test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
