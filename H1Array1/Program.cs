4210a62754a626633bef7a92534155fcdba8c65b
Sebastian Lind Karlsen <seba6474@zbc.dk>
2020-01-29 09:48:49 +0100

    Done

--- H1Array1/Program.cs
+++ H1Array1/Program.cs
@@ -10,6 +10,16 @@ namespace H1Array1
     {
         static void Main(string[] args)
         {
+            int[] tal = new int[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                tal[i] = i;
+            }
+            Console.WriteLine(tal[5]);
+            tal[5] *= 2;
+
+            Console.ReadKey();
         }
     }
 }
